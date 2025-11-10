9c1dd28bf1132126eebc028ea93f940a628faefc
teejayidrees <ahmadgbodoti@gmail.com>
2025-11-10 15:15:04 +0100

    please

--- src/components/AuthPage/AuthPage.jsx
+++ src/components/AuthPage/AuthPage.jsx
@@ -7,57 +7,52 @@ import { Link } from "react-router-dom";
 import "./AuthPage.css";
 const AuthPage = () => {
   return (
-    <div className="auth-page-container">
-      <Link to="/">
-        <p>Home</p>
-      </Link>
-      <Link to="/payment">
-        <p>Payment (page2)</p>
-      </Link>
-
-      <div className="brand-name-div">
-        <img src={logo} alt="logo svg" />
-        <p>dotwork</p>
-      </div>
-      <h1 className="header">Create your Account</h1>
-      <div className="oauth-div">
-        <button>
-          <img src={googleLogo} /> Google
-        </button>
-        <button>
-          <img src={facebookLogo} />
-          Facebook
-        </button>
-      </div>
-      <div className="email-divider">
-        <hr className="email-line" />
-        <p>or continue with email</p>
-        <hr className="email-line" />
-      </div>
-      <div className="form-container">
-        <div className="input-div-auth">
-          <Mail className="icon-auth" />
-          <input type="email" name="Email" placeholder="Email" />
+    <div className="big">
+      <div className="auth-page-container">
+        <div className="brand-name-div">
+          <img src={logo} alt="logo svg" />
+          <p>dotwork</p>
         </div>
-        <div className="input-div-auth">
-          <Lock className="icon-auth" />
-          <input type="password" name="Password" placeholder="Password" />
+        <h1 className="header">Create your Account</h1>
+        <div className="oauth-div">
+          <button>
+            <img src={googleLogo} /> Google
+          </button>
+          <button>
+            <img src={facebookLogo} />
+            Facebook
+          </button>
         </div>
-      </div>
-      <div className="options-div">
-        <div className="remember-me">
-          <input type="checkbox" name="Remember me" width={"50px"} />
-          <label htmlFor="Remember me"> Remember me</label>
+        <div className="email-divider">
+          <hr className="email-line" />
+          <p>or continue with email</p>
+          <hr className="email-line" />
         </div>
-        <p>
-          <a href="/">Forgot Password?</a>
-        </p>
-      </div>
-      <div className="action-div">
-        <button className="create-button">Create</button>
-        <p className="login-link">
-          Already have an account? <a href="/">Login</a>
-        </p>
+        <div className="form-container">
+          <div className="input-div-auth">
+            <Mail className="icon-auth" />
+            <input type="email" name="Email" placeholder="Email" />
+          </div>
+          <div className="input-div-auth">
+            <Lock className="icon-auth" />
+            <input type="password" name="Password" placeholder="Password" />
+          </div>
+        </div>
+        <div className="options-div">
+          <div className="remember-me">
+            <input type="checkbox" name="Remember me" width={"50px"} />
+            <label htmlFor="Remember me"> Remember me</label>
+          </div>
+          <p>
+            <a href="/">Forgot Password?</a>
+          </p>
+        </div>
+        <div className="action-div">
+          <button className="create-button">Create</button>
+          <p className="login-link">
+            Already have an account? <a href="/">Login</a>
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -8,36 +8,37 @@ import "./Carousel.css";
 import ChatPage from "../ChatPage/ChatPage";
 const Carousel = () => {
   return (
-    <div className="top-div">
-      <div className="carousel-wrapper">
-        <div className="circle-container">
-          <img
-            src={circleGradient}
-            className="circle-gradient"
-            alt="gradient circle"
-          />
-          <img src={circle} className="circle" alt="center circle" />
-          <div className="icon icon--1">
-            <img src={icon2} alt="icon 1" />
+    <div className="master-div">
+      <div className="top-div">
+        <div className="carousel-wrapper">
+          <div className="circle-container">
+            <img
+              src={circleGradient}
+              className="circle-gradient"
+              alt="gradient circle"
+            />
+            <img src={circle} className="circle" alt="center circle" />
+            <div className="icon icon--1">
+              <img src={icon2} alt="icon 1" />
+            </div>
+            <div className="icon icon--2">
+              <img src={icon1} alt="icon 2" />
+            </div>
+            <div className="icon icon--3">
+              <img src={icon3} alt="icon 3" />
+            </div>
+            <ChatPage />
           </div>
-          <div className="icon icon--2">
-            <img src={icon1} alt="icon 2" />
-          </div>
-          <div className="icon icon--3">
-            <img src={icon3} alt="icon 3" />
-          </div>
-          <ChatPage />
         </div>
-      </div>
-
-      <div className="white-text">
-        <h1>Connect with every application</h1>
-        <p>Everything you need in a customizable dashboard</p>
-      </div>
-      <div className="carousel-pointer">
-        <span style={{ backgroundColor: "#FFFFFF" }}></span>
-        <span style={{ backgroundColor: "#0D7DE7DD" }}></span>
-        <span style={{ backgroundColor: "#0D7DE7DD" }}></span>
+        <div className="white-text">
+          <h1>Connect with every application</h1>
+          <p>Everything you need in a customizable dashboard</p>
+        </div>
+        <div className="carousel-pointer">
+          <span style={{ backgroundColor: "#FFFFFF" }}></span>
+          <span style={{ backgroundColor: "#0D7DE7DD" }}></span>
+          <span style={{ backgroundColor: "#0D7DE7DD" }}></span>
+        </div>
       </div>
     </div>
   );
